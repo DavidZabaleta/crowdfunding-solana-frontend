3bd54d0115dda8e78306bde2a533ecdfaef1d4c4
David Zabaleta <david.zabaleta@publicisgroupe.com>
2022-07-20 21:48:56 -0500

    Create a campaign

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import idl from "./idl.json"
 import {useCallback, useEffect, useState} from "react";
+import {clusterApiUrl, Connection, PublicKey} from "@solana/web3.js";
+import {AnchorProvider, Program, utils, web3} from "@project-serum/anchor";
+import {Buffer} from "buffer"
+
+window.Buffer = Buffer;
+
+const programId = new PublicKey(idl.metadata.address);
+const network = clusterApiUrl("devnet");
+const opts = {
+    preflightCommitment: "processed"
+};
+const { SystemProgram } = web3;
 
 const App = () => {
     const [walletAddress, setWalletAddress] = useState(null);
     const { solana } = window;
+
+    const getProvider = () => {
+        const connection = new Connection(network, opts.preflightCommitment);
+
+        return new AnchorProvider(connection, solana, opts.preflightCommitment);
+    }
 
     const checkIfWalletIsConnected = () => {
         try {
@@ -32,7 +51,34 @@ const App = () => {
         setWalletAddress(response.publicKey.toString());
     }
 
+    const createCampaign = async () => {
+        try {
+            const provider = getProvider();
+            const program = new Program(idl, programId, provider);
+            const [campaign] = await PublicKey.findProgramAddress(
+                [
+                    utils.bytes.utf8.encode("CAMPAIGN_DEMO"),
+                    provider.wallet.publicKey.toBuffer()
+                ],
+                program.programId
+            );
+
+            await program.rpc.create("campaign name", "campaign description", {
+                accounts: {
+                    campaign,
+                    user: provider.wallet.publicKey,
+                    systemProgram: SystemProgram.programId
+                }
+            });
+
+            console.log("Create new campaign with address:", campaign.toString());
+        } catch (e) {
+            console.error("Error creating campaign account:", e);
+        }
+    };
+
     const renderNotConnectedContainer = () => <button onClick={connectNewWallet}>Connect Wallet</button>;
+    const renderConnectedContainer = () => <button onClick={createCampaign}>Create a campaign…</button>;
 
     useEffect(() => {
         const onLoad = async () => {
@@ -48,6 +94,7 @@ const App = () => {
         <div className="App">
             <p>{checkIfWalletIsConnected()}</p>
             {(!walletAddress && solana) && renderNotConnectedContainer()}
+            {walletAddress && renderConnectedContainer()}
         </div>
     );
 }
